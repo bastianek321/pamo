34ae518aed8e1ccae8d778e101c84cd9ca950b76
bastianek321 <czaj.sebastian@gmail.com>
2022-03-19 22:38:56 +0100

    add BMI calculator

---
+++ Tipper/app/src/main/java/com/example/tipper/MainActivity.java
@@ -0,0 +1,125 @@
+package com.example.tipper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable; // for EditText event handling
+import android.text.TextWatcher; // EditText listener
+import android.widget.EditText; // for bill amount input
+import android.widget.TextView; // for displaying text
+
+import java.text.NumberFormat;
+
+
+public class MainActivity extends AppCompatActivity {
+    private static final NumberFormat numberFormat =
+            NumberFormat.getNumberInstance();
+
+    private double mass = 0.0;
+    private double height = 0.0;
+    private TextView massTextView;
+    private TextView heightTextView;
+    private TextView bmiTextView;
+
+    // called when the activity is first created
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState); // call superclass onCreate
+        setContentView(R.layout.activity_main); // inflate the GUI
+
+        // get references to programmatically manipulated TextViews
+        massTextView = (TextView) findViewById(R.id.massTextView);
+        heightTextView = (TextView) findViewById(R.id.heightTextView);
+        bmiTextView = (TextView) findViewById(R.id.bmiTextView);
+        bmiTextView.setText(numberFormat.format(0));
+
+        // set amountEditText's TextWatcher
+        EditText amountEditText =
+                (EditText) findViewById(R.id.massEditText);
+        amountEditText.addTextChangedListener(massEditTextWatcher);
+
+        EditText heightEditText =
+                (EditText) findViewById(R.id.heightEditText);
+        heightEditText.addTextChangedListener(heightEditTextWatcher);
+    }
+
+    // calculate and display tip and total amounts
+    private void calculate() {
+
+        // calculate the tip and total
+        double bmi = mass/Math.pow(height, 2);
+
+        // display tip and total formatted as currency
+        bmiTextView.setText(Double.toString(bmi));
+    }
+
+    // listener object for the SeekBar's progress changed events
+    private final TextWatcher heightEditTextWatcher = new TextWatcher() {
+        // called when the user modifies the bill amount
+        @Override
+        public void onTextChanged(CharSequence s, int start,
+                                  int before, int count) {
+
+            try { // get bill amount and display currency formatted value
+                height = Double.parseDouble(s.toString());
+                heightTextView.setText(Double.toString(height));
+            }
+            catch (NumberFormatException e) { // if s is empty or non-numeric
+                heightTextView.setText("");
+                height = 0.0;
+            }
+
+            calculate(); // update the tip and total TextViews
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) { }
+
+        @Override
+        public void beforeTextChanged(
+                CharSequence s, int start, int count, int after) { }
+    };
+
+    // listener object for the EditText's text-changed events
+    private final TextWatcher massEditTextWatcher = new TextWatcher() {
+        // called when the user modifies the bill amount
+        @Override
+        public void onTextChanged(CharSequence s, int start,
+                                  int before, int count) {
+
+            try { // get bill amount and display currency formatted value
+                mass = Double.parseDouble(s.toString());
+                massTextView.setText(Double.toString(mass));
+            }
+            catch (NumberFormatException e) { // if s is empty or non-numeric
+                massTextView.setText("");
+                mass = 0.0;
+            }
+
+            calculate(); // update the tip and total TextViews
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) { }
+
+        @Override
+        public void beforeTextChanged(
+                CharSequence s, int start, int count, int after) { }
+    };
+}
+
+
+/*************************************************************************
+ * (C) Copyright 1992-2016 by Deitel & Associates, Inc. and               *
+ * Pearson Education, Inc. All Rights Reserved.                           *
+ *                                                                        *
+ * DISCLAIMER: The authors and publisher of this book have used their     *
+ * best efforts in preparing the book. These efforts include the          *
+ * development, research, and testing of the theories and programs        *
+ * to determine their effectiveness. The authors and publisher make       *
+ * no warranty of any kind, expressed or implied, with regard to these    *
+ * programs or to the documentation contained in these books. The authors *
+ * and publisher shall not be liable in any event for incidental or       *
+ * consequential damages in connection with, or arising out of, the       *
+ * furnishing, performance, or use of these programs.                     *
+ *************************************************************************/
